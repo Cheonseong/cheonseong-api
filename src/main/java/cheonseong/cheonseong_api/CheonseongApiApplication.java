545ae20c3c9ac923ed388637782b3b22ddcbf657
Yechan Lee <yechan24680@gmail.com>
2024-07-13 22:36:48 +0900

    chore: first commit

---
+++ src/main/java/cheonseong/cheonseong_api/CheonseongApiApplication.java
@@ -0,0 +1,13 @@
+package cheonseong.cheonseong_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheonseongApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheonseongApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cheonseong/cheonseong_api/CheonseongApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cheonseong.cheonseong_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheonseongApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
